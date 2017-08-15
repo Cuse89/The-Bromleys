1efc978e514031e0fcf449e9113737e535ee944b
Marcus Guthrie <marcusguthrie111@gmail.com>
2017-08-15 20:54:04 +0100

    Create/remove Gig function

--- the_bromleys.js
+++ the_bromleys.js
@@ -9,7 +9,7 @@ var sundaySessions = {
   date: '7th',
   month: 'May',
   time: '16:00',
-  eventName: 'Sunday Sessions present the Bromleys',
+  eventTitle: 'Sunday Sessions present the Bromleys',
   venue: 'The Grayston Unity',
   town: 'Halifax',
   description: 'Sunday sessions at the Grayston Unity. Free entry. In the back yard (weather permitting).',
@@ -21,7 +21,7 @@ var todFolkFest = {
   date: '30th',
   month: 'April',
   time: '18:00',
-  eventName: 'The Bromleys at Todmorden Folk Festival',
+  eventTitle: 'The Bromleys at Todmorden Folk Festival',
   venue: 'Fielden Centre',
   town: 'Todmorden',
   description: "We're very excited to be playing the closing concert at this year's Todmorden Folk Festival with Murston Bapchild and The Braxton Hicks and Isaac (Trickstar Entertainment). For further information and tickets (£10) please go to todfolkfest.co.uk.",
@@ -30,9 +30,37 @@ var todFolkFest = {
 
 /* look into creating objects from form fields */
 
-createGig(sundaySessions);
-createGig(todFolkFest);
 
-function createGig(event) {
-  $('.list-group').append(`<a target = '_blank' href='${event.website}' class="list-group-item"><h4 class='list-group-item-heading'> ${event.day} ${event.date} ${event.month} ${event.time} - ${event.eventName} -  ${event.venue}, ${event.town}</h4><p class='list-group-item-text'>${event.description}</p></a>`);
+$("#eventForm").submit(function(event) {
+  var x = ( $(this).serializeArray() );
+  event.preventDefault();
+
+  var newGig = {
+    day: x[0].value,
+    date: x[1].value,
+    month: x[2].value,
+    time: x[3].value,
+    eventTitle: x[4].value,
+    venue: x[5].value,
+    town: x[6].value,
+    description: x[7].value,
+    website: x[8].value
+  }
+  createGig(newGig)
+  alert('Gig Created!')
+});
+
+createGig(todFolkFest);
+createGig(sundaySessions);
+
+function createGig(gig) {
+  $('#gigList').append(`<div class="list-group-item gigEntry"><a target='_blank' href='${gig.website}'><h4 class='list-group-item-heading'> ${gig.day} ${gig.date} ${gig.month} ${gig.time} - ${gig.eventTitle} -  ${gig.venue}, ${gig.town}</h4><p class='list-group-item-text'>${gig.description}</p></a></div>`);
 }
+
+$('#earliestGigDelete').click(function(){
+  $('.gigEntry').first().remove();
+})
+
+$('#latestGigDelete').click(function(){
+  $('.gigEntry').last().remove();
+})
